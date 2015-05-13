f8f92a38773822ce53455e9897b90fe8e2fc5ddb
Scott Willson <scott.willson@gmail.com>
2015-05-12 22:30:14 -0700

    Add naive update route

--- app.js
+++ app.js
@@ -5,13 +5,23 @@ var redis = require('redis'),
     client = redis.createClient();
 
 app.get('/api/v1/articles/1', showArticle);
+app.post('/api/v1/articles/1', updateArticle);
 
 function showArticle(req, res) {
-  client.get('article-version', function(err, articleVersion) {
+  client.get('article-1-version', function(err, articleVersion) {
     var articlePath = 'public/article-' + articleVersion + '.html';
     fs.readFile(articlePath, function(err, data) {
       if (err) return res.status(500).send(err);
       res.send(data);
+    });
+  });
+}
+
+function updateArticle(req, res) {
+  client.get('article-1-version', function(err, articleVersion) {
+    var articlePath = 'public/article-' + articleVersion + '.html';
+    fs.writeFile(articlePath, req.body.html, function(err, data) {
+      res.send('OK');
     });
   });
 }
