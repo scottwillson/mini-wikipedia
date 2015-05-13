914ce169533e78392cc20a6d8a07f437e7a95ec5
Scott Willson <scott.willson@gmail.com>
2015-05-12 22:20:57 -0700

    Add version to HTML file for optimistic locking

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
+var redis = require('redis'),
+    client = redis.createClient();
 
 app.get('/api/v1/articles/1', function(req, res) {
-  fs.readFile('public/article.html', function(err, data) {
-    if (err) return res.status(500).send(err);
-    res.send(data);
+  client.get('article-version', function(err, articleVersion) {
+    var articlePath = 'public/article-' + articleVersion + '.html';
+    fs.readFile(articlePath, function(err, data) {
+      if (err) return res.status(500).send(err);
+      res.send(data);
+    });
   });
 });
 
