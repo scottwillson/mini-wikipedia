cf8ca0e33aed742e3daca69e267dbbd64303d4fd
Scott Willson <scott.willson@gmail.com>
2015-05-12 22:11:03 -0700

    Add editing traffic (which fails on 404)

--- session.rb
+++ session.rb
@@ -1,4 +1,15 @@
 CelluloidBenchmark::Session.define do
-  benchmark :article_show, 0.5
-  get "http://localhost:3000/api/v1/articles/1"
+  20.times do
+    if rand < 0.01
+      benchmark :article_show, 0.5
+      page = get("http://localhost:3000/api/v1/articles/1")
+      version = page["article-version"]
+
+      benchmark :article_update, 0.5
+      post "http://localhost:3000/api/v1/articles/1", { version: version, html: "<html/>"}
+    else
+      benchmark :article_show, 0.5
+      get "http://localhost:3000/api/v1/articles/1"
+    end
+  end
 end
